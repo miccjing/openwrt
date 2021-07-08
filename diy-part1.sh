a9ddce8cb65132ce14537ad30a6dd21028dd13c5
micc <36817721+miccjing@users.noreply.github.com>
2021-07-08 10:36:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,6 @@
 
 # Add a feed source
 git clone https://github.com/miccjing/luci-theme-atmaterial package/lean/luci-theme-atmaterial
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
