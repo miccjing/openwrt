48332333477e82dcde7e76e7c7720283f3d2c08d
micc <36817721+miccjing@users.noreply.github.com>
2024-09-11 19:40:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-git clone https://github.com/miccjing/luci-theme-atmaterial package/lean/luci-theme-atmaterial
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
+#git clone https://github.com/miccjing/luci-theme-atmaterial package/lean/luci-theme-atmaterial
+sed -i '$a src-git 5gmodem https://github.com/Siriling/5G-Modem-Support.git' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
